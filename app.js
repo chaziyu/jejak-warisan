0f8a3680e5ef95b5f52c8be17a7e8c532c531e96
CHA ZI YU <23120943@siswa.um.edu.my>
2025-11-14 00:30:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -178,8 +178,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Initialize Map
     const map = L.map('map').setView([3.1483, 101.6938], 16);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap'
+
+    // USE THIS CLEAN STYLE (CartoDB Positron)
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
     }).addTo(map);
 
     // --- KML HERITAGE ZONE POLYGON ---
